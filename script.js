ad81d4ba99d2e26165dbf418af49b852415b0d25
rishbh2168 <rishabhrgupta2168@gmail.com>
2023-08-29 11:40:49 +0530

    second commit

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Har Har Gange - Arjit", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Baarishein - Anuv", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Chidiya - Vilen", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Heeriye Heeriye - Jaslon", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "No Reason - Parmish", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Pasoori - Gill", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
     {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
     {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
     {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
